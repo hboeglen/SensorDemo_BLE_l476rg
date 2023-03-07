f37100dada81814a52c875663b7c997ed7f9be5a
hboeglen <60876763+hboeglen@users.noreply.github.com>
2023-03-07 16:23:07 +0100

    Delete STM32CubeIDE/Debug directory

--- STM32CubeIDE/Debug/Application/User/bsp.d
+++
@@ -1,2 +0,0 @@
-Application/User/bsp.o: \
- /home/herve/Documents/stm32_projects/SensorDemo_BLE_l476rg/Src/bsp.c
--- STM32CubeIDE/Debug/Middlewares/BlueNRG-2/Wireless/BlueNRG-2/HCI_TL/Basic/bluenrg1_devConfig.d
+++
@@ -1,8 +0,0 @@
-Middlewares/BlueNRG-2/Wireless/BlueNRG-2/HCI_TL/Basic/bluenrg1_devConfig.o: \
- /home/herve/Documents/stm32_projects/SensorDemo_BLE_l476rg/Middlewares/ST/BlueNRG-2/hci/bluenrg1_devConfig.c \
- ../../Middlewares/ST/BlueNRG-2/includes/bluenrg_utils.h \
- ../../Middlewares/ST/BlueNRG-2/includes/compiler.h \
- ../../Middlewares/ST/BlueNRG-2/includes/ble_types.h
-../../Middlewares/ST/BlueNRG-2/includes/bluenrg_utils.h:
-../../Middlewares/ST/BlueNRG-2/includes/compiler.h:
-../../Middlewares/ST/BlueNRG-2/includes/ble_types.h:
